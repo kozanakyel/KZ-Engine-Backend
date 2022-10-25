7289e8773e202d186d3f1e9a3e1a6f905648332e
kozan <kozan@LAPTOP-83OBPT96.localdomain>
2022-10-25 22:32:45 +0300

    added twitter collection class and get every week tweets for specific data + last 3 moths work added for this project

---
+++ KZ_project_setup/data_manipulation.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+import os
+from indicators import Indicators
+import yfinance as yf
+from candlestick_features import *
+
+
+class DataManipulation():
+    def __init__(self, symbol: str, source: str, range: list, period=None, interval=None, 
+                                start_date=None, end_date=None, scale=1):
+        self.symbol = symbol
+        self.source = source
+        self.scale = scale
+        self.range = [i*scale for i in range]
+        self.period = period
+        self.interval = interval 
+        self.start_date = start_date
+        self.end_date = end_date
+        self.df = self.create_data_one(self.symbol, self.source, self.period, self.interval)
+
+    
+    def create_data_one(self, symbol, source, period=None, interval=None) -> pd.DataFrame():
+        path_df = './outputs/data_ind/'+symbol
+        pure_data = './pure_data/'+symbol
+        pure_file = f'{symbol}_{period}_{interval}.csv'
+        file = f'{symbol}_df_{period}_{interval}.csv'
+        
+        if os.path.exists(os.path.join(path_df, file)):
+            self.df = pd.read_csv(os.path.join(path_df, file))
+            self.df['Datetime'] = pd.to_datetime(self.df['Datetime'])
+            self.df = self.df.set_index('Datetime')
+
+        elif source == 'yahoo' and period != None:
+            self.df = yf.download(symbol, period=period, interval=interval)
+            self.df['Datetime'] = self.df.index
+            self.df = self.df.set_index('Datetime')
+            self.write_file_data(self.df, pure_data, pure_file)
+
+            ind = Indicators(self.df, self.range)
+            create_candle_columns(self.df, candle_names=candle_names, candle_rankings=candle_rankings)
+            create_candle_label(self.df)
+            self.df = self.df.drop(columns=['candlestick_match_count'], axis=1)
+
+            if 'Adj Close' in self.df.columns.to_list():
+                self.df = self.df.drop(columns=['Adj Close'], axis=1)
+            self.create_binary_feature_label()
+            self.df.dropna(inplace= True, how='any')
+            self.write_file_data(self.df, path_df, file) 
+            
+        return self.df
+        
+    
+    def create_binary_feature_label(self) -> None:
+        self.df['d_r'] = self.df['Close'].pct_change()
+        self.df['temp'] = self.df['d_r']*10000
+        self.df['feature_label'] = np.where(self.df['temp'].ge(0), 1, 0)
+        self.df['feature_label'] = self.df['feature_label'].shift(-1)
+        self.df = self.df.drop(columns=['temp'], axis=1)
+
+    def write_file_data(self, df: pd.DataFrame(), pathdf: str, filedf: str) -> None:
+        if not os.path.exists(os.path.join(pathdf, filedf)):
+            os.makedirs(pathdf, exist_ok=True)
+            with open(os.path.join(pathdf, filedf), mode='a'): pass 
+            df.to_csv(os.path.join(pathdf, filedf))
+    
---
+++ KZ_project_setup/twitter_collection.py
@@ -0,0 +1,124 @@
+import re, tweepy, datetime, time, json, csv
+from tweepy import OAuthHandler
+import tweepy
+import pandas as pd
+import os
+from dotenv import load_dotenv
+import datetime
+from datetime import datetime as dt 
+from datetime import timedelta as td
+
+load_dotenv()
+
+access_token=os.getenv('TW_access_token')
+access_token_secret=os.getenv('TW_access_token_secret')
+consumer_key=os.getenv('TW_consumer_key')
+consumer_secret=os.getenv('TW_consumer_secret')
+MY_BEARER_TOKEN=os.getenv('TW_BEARER_TOKEN')
+
+class TwitterCollection():
+    def __init__(self, access_token=access_token, access_token_secret=access_token_secret,
+                    consumer_key=consumer_key, consumer_secret=consumer_secret, bearer_token=MY_BEARER_TOKEN):
+        self.access_token = access_token
+        self.access_token_secret=access_token_secret
+        self.consumer_key=consumer_key
+        self.consumer_secret=consumer_secret
+        self.bearer_token=bearer_token
+        self.connect_twitter()
+        self.client = tweepy.Client(bearer_token=self.bearer_token)
+
+    def connect_twitter(self):
+        # attempt authentication
+        try:
+            # create OAuthHandler object
+            auth = OAuthHandler(self.consumer_key, self.consumer_secret)
+            # set access token and secret
+            auth.set_access_token(self.access_token, self.access_token_secret)
+            # create tweepy API object to fetch tweets
+            api = tweepy.API(auth, wait_on_rate_limit=True)
+            #data = self.api.rate_limit_status()
+            print("Authentication Successfull")
+        except:
+            print("Error: Authentication Failed")
+
+    def get_tweets(self, search_query: str, lang: str, start_time: str, end_time: str):
+        query = f"#{search_query} lang:{lang} -is:retweet"
+        tweets = self.client.search_recent_tweets(query=query,
+                                         start_time=start_time,
+                                         end_time=end_time,
+                                         tweet_fields = ["created_at", "text", "source"],
+                                         user_fields = ["name", "username", "location", "verified", "description"],
+                                         max_results = 100,
+                                         expansions='author_id'
+                                        ) 
+        return tweets
+
+    def converts_tweets_pd(self, tweets: dict) -> pd.DataFrame():
+        tweet_info_ls = []
+        # iterate over each tweet and corresponding user details
+        for user in tweets.includes.get('users', ''):
+            for tweet, user in zip(tweets.data, tweets.includes['users']):
+                tweet_info = {
+                'created_at': tweet.created_at,
+                'text': tweet.text,
+                'source': tweet.source,
+                'name': user.name,
+                'username': user.username,
+                'location': user.location,
+                'verified': user.verified,
+                'description': user.description
+                }
+                tweet_info_ls.append(tweet_info)
+        # create dataframe from the extracted records
+        tweets_df = pd.DataFrame(tweet_info_ls)
+        return tweets_df
+
+
+    def get_tweets_with_interval(self, hashtag: str, lang: str, start_time=None, finish_time=None, hour=24, interval =1) -> pd.DataFrame():
+        now = datetime.datetime.now(datetime.timezone.utc)
+        if start_time == None:
+            start_time = now - td(hours=hour)
+        if finish_time == None:
+            finish_time = now
+        end_time = start_time + td(hours=interval)
+
+        result_tweets = pd.DataFrame()
+        while end_time <= finish_time - td(hours=1):
+            temp_tweets = self.get_tweets(hashtag, lang, start_time.isoformat(), end_time.isoformat())
+            df_temp_tweets = self.converts_tweets_pd(temp_tweets)
+            result_tweets = pd.concat([df_temp_tweets, result_tweets], ignore_index=True)
+            start_time = start_time + td(hours=1)
+            end_time = end_time + td(hours=1)
+        return result_tweets
+
+
+    def write_tweets_csv(self, df: pd.DataFrame(), pathdf: str, filedf: str) -> None:
+        if not os.path.exists(os.path.join(pathdf, filedf)):
+            os.makedirs(pathdf, exist_ok=True)
+            with open(os.path.join(pathdf, filedf), mode='a'): pass
+            df['index_col'] = df.index 
+            df = df.set_index('index_col')
+            df.to_csv(os.path.join(pathdf, filedf))
+        else:
+            temp_tweets = pd.read_csv(os.path.join(pathdf, filedf))
+            temp_tweets = pd.concat([df, temp_tweets], ignore_index=True)
+            temp_tweets.to_csv(os.path.join(pathdf, filedf))
+
+    def get_tweets_df(self, symbol: str, pathdf: str, filedf: str) -> pd.DataFrame():
+        if not os.path.exists(os.path.join(pathdf, filedf)):
+            print(f'This symbols {symbol} tweet not have')
+            return
+        else:
+            temp_tweets = pd.read_csv(os.path.join(pathdf, filedf))
+        return temp_tweets
+
+    def throw_unnamed_cols(self, query: str) -> None:
+        path_df = f'./tweets_data/{query}/'
+        file_df = f'{query}_tweets.csv'
+        df_tweet = self.get_tweets_df('sasa', path_df, file_df)
+        index_columns_list = ['created_at', 'text', 'source', 'name', 'username',
+              'location', 'verified', 'description']
+        df_tweet['index_col'] = df_tweet.index
+        df_tweet = df_tweet.set_index('index_col')
+        df_tweet = df_tweet.drop([i for i in df_tweet.columns.to_list() if i not in index_columns_list], axis=1)
+        df_tweet.to_csv(os.path.join(path_df, file_df))
