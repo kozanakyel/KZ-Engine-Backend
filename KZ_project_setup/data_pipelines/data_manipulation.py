d866c839e32eb7b6941d5db0fd426c333cf7bd3b
kozan <kozan@LAPTOP-83OBPT96.localdomain>
2022-11-09 22:02:58 +0300

    data manipulation and indicators classes edited

--- KZ_project_setup/data_pipelines/data_manipulation.py
+++ KZ_project_setup/data_pipelines/data_manipulation.py
@@ -7,12 +7,12 @@ from technical_analysis.candlestick_features import *
 
 
 class DataManipulation():
-    def __init__(self, symbol: str, source: str, range: list, period=None, interval=None, 
+    def __init__(self, symbol: str, source: str, range_list: list, period=None, interval=None, 
                                 start_date=None, end_date=None, scale=1, prefix_path='.'):
         self.symbol = symbol
         self.source = source
         self.scale = scale
-        self.range = [i*scale for i in range]
+        self.range_list = [i*scale for i in range_list]
         self.period = period
         self.interval = interval 
         self.start_date = start_date
@@ -49,19 +49,22 @@ class DataManipulation():
             self.df = self.df.set_index('Datetime')
 
         elif source == 'yahoo' and period != None:
-            self.df = yf.download(symbol, period=period, interval=interval)
+            df_download = yf.download(symbol, period=period, interval=interval)
+            self.df = df_download.copy()
             self.df['Datetime'] = self.df.index
             self.df = self.df.set_index('Datetime')
-            if self.df.shape[0] > self.range[-1]:
+            if self.df.shape[0] > self.range_list[-1]:
                 self.write_file_data(self.df, pure_data, pure_file)
                 
-                ind = Indicators(self.df, self.range)
+                indicators = Indicators(self.df, self.range_list)
+                indicators.create_ind_features()
+                self.df = indicators.df.copy()
                 create_candle_columns(self.df, candle_names=candle_names, candle_rankings=candle_rankings)
                 create_candle_label(self.df)
                 self.df = self.df.drop(columns=['candlestick_match_count'], axis=1)
 
                 if 'Adj Close' in self.df.columns.to_list():
-                    self.df = self.df.drop(columns=['Adj Close'], axis=1)
+                    self.df = self.df.rename(columns={"Adj Close": "Adj_close"})
                 self.create_binary_feature_label()
                 self.df.dropna(inplace= True, how='any')
                 self.write_file_data(self.df, path_df, file) 
@@ -86,7 +89,66 @@ class DataManipulation():
             df.to_csv(os.path.join(pathdf, filedf))
 
     def extract_features(self):
-        df = self.df.copy()
+        path_df = self.prefix_path+'/data/outputs/feature_data/'+self.symbol
+        file_df = f'{self.symbol}_df_{self.period}_{self.interval}.csv'
+        
+        if os.path.exists(os.path.join(path_df, file_df)):
+            sample = pd.read_csv(os.path.join(path_df, file_df))
+            sample['Datetime'] = pd.to_datetime(sample['Datetime'])
+            sample = sample.set_index('Datetime')
+        else:
+            df = self.df.copy()
+            sample = self.pattern_helper_for_extract_feature(df)        
+            self.norm_features_ind(sample, df, 'ema', self.range_list, df.Close)
+            self.norm_features_ind(sample, df, 'mfi', self.range_list, 100)
+            self.norm_features_ind(sample, df, 'sma', self.range_list, df.Close)
+            self.norm_features_ind(sample, df, 'wma', self.range_list, df.Close)
+            self.norm_features_ind(sample, df, 'tema', self.range_list, df.Close)
+            self.norm_features_ind(sample, df, 'kama', self.range_list, df.Close)
+            self.norm_features_ind(sample, df, 'rsi', self.range_list, 100)
+            self.norm_adx_ind(sample, df, self.range_list)
+            sample['st_ich'] = (df['ich_tline'] > df['ich_kline']).astype(int)
+        
+            sample['month'] = sample.index.month
+            sample['weekday'] = sample.index.weekday
+            sample['is_quarter_end'] = sample.index.is_quarter_end*1
+            sample['candle'] = df.candle_label
+            sample['vol_delta'] = sample['Volume'].pct_change()
+            self.add_lags(sample, df, 5)
+            sample['log_return'] = df.log_rt
+            sample.drop(columns=['Close', 'Volume'], axis=1, inplace=True)
+            self.write_file_data(sample, path_df, file_df)
+
+        return sample
+
+    def norm_features_ind(self, sampledf, df, ind, range_list, dividend):
+        k = 0
+        for i in range_list:
+            sampledf[f'st_{ind}_{i}'] = (dividend - df[f'{ind}_{i}']) / dividend
+            if k % 2 == 1:
+                sampledf[f'st_cut_{ind}_{range_list[k-1]}_{range_list[k]}'] = \
+                        (df[f'{ind}_{range_list[k-1]}'] > df[f'{ind}_{range_list[k]}']).astype(int)
+            k += 1
+
+    def norm_adx_ind(self, sampledf, df, range_list):
+        for i in range_list:
+            sampledf[f'st_adx_{i}'] = (100 - df[f'adx_{i}']) / 100
+            pattern1 = df[f'adx_{i}'] < 50
+            pattern2 = df[f'dmi_up_{i}'] > df[f'dmi_down_{i}']
+            sampledf[f'st_adxdmi_{i}'] = (pattern2).astype(int) + pattern1
+
+    def add_lags(self, sampledf: pd.DataFrame(), df: pd.DataFrame(), lag_numbers: int) -> None:
+        i = 1
+        while i < lag_numbers:
+            sampledf[f'lag_{i}'] = (df.log_rt.shift(i) > 0).astype(int)
+            i += 1
+
+    def compound_annual_growth_rate(df: pd.DataFrame(), close) -> float:
+        norm = df[close][-1]/df[close][0]
+        cagr = (norm)**(1/((df.index[-1] - df.index[0]).days / 365.25)) - 1
+        return cagr
+
+    def pattern_helper_for_extract_feature(self, df):
         sample  = df[['Open','High','Low','Close','Volume']].copy()
         sample.drop(columns=['Open','High','Low'], axis=1, inplace=True)
         sample['st_hisse'] = np.nan
@@ -142,52 +204,6 @@ class DataManipulation():
                 sample['st_hisse'].iloc[index] = 1
             else:
                 sample['st_hisse'].iloc[index] = 0
-        
-        self.norm_features_ind(sample, df, 'ema', self.range, df.Close)
-        self.norm_features_ind(sample, df, 'mfi', self.range, 100)
-        self.norm_features_ind(sample, df, 'sma', self.range, df.Close)
-        self.norm_features_ind(sample, df, 'wma', self.range, df.Close)
-        self.norm_features_ind(sample, df, 'tema', self.range, df.Close)
-        self.norm_features_ind(sample, df, 'kama', self.range, df.Close)
-        self.norm_features_ind(sample, df, 'rsi', self.range, 100)
-        self.norm_adx_ind(sample, df, self.range)
-        sample['st_ich'] = (df['ich_tline'] > df['ich_kline']).astype(int)
-        
-        sample['month'] = sample.index.month
-        sample['weekday'] = sample.index.weekday
-        sample['is_quarter_end'] = sample.index.is_quarter_end*1
-        sample['candle'] = df.candle_label
-        sample['vol_delta'] = sample['Volume'].pct_change()
-        self.add_lags(sample, df, 5)
-        sample['log_return'] = df.log_rt
-        sample.drop(columns=['Close', 'Volume'], axis=1, inplace=True)
 
         return sample
-
-    def norm_features_ind(self, sampledf, df, ind, range, dividend):
-        k = 0
-        for i in range:
-            sampledf[f'st_{ind}_{i}'] = (dividend - df[f'{ind}_{i}']) / dividend
-            if k % 2 == 1:
-                sampledf[f'st_cut_{ind}_{range[k-1]}_{range[k]}'] = \
-                        (df[f'{ind}_{range[k-1]}'] > df[f'{ind}_{range[k]}']).astype(int)
-            k += 1
-
-    def norm_adx_ind(self, sampledf, df, range):
-        for i in range:
-            sampledf[f'st_adx_{i}'] = (100 - df[f'adx_{i}']) / 100
-            pattern1 = df[f'adx_{i}'] < 50
-            pattern2 = df[f'dmi_up_{i}'] > df[f'dmi_down_{i}']
-            sampledf[f'st_adxdmi_{i}'] = (pattern2).astype(int) + pattern1
-
-    def add_lags(self, sampledf: pd.DataFrame(), df: pd.DataFrame(), lag_numbers: int) -> None:
-        i = 1
-        while i < lag_numbers:
-            sampledf[f'lag_{i}'] = (df.log_rt.shift(i) > 0).astype(int)
-            i += 1
-
-    def compound_annual_growth_rate(df: pd.DataFrame(), close) -> float:
-        norm = df[close][-1]/df[close][0]
-        cagr = (norm)**(1/((df.index[-1] - df.index[0]).days / 365.25)) - 1
-        return cagr
     
--- KZ_project_setup/technical_analysis/indicators.py
+++ KZ_project_setup/technical_analysis/indicators.py
@@ -6,77 +6,76 @@ import numpy as np
 
 class Indicators():
 
-    def __init__(self, df: pd.DataFrame(), range: list):
-        self.range = range
-        self.df = df
-        self.create_ind_features()
+    def __init__(self, df: pd.DataFrame(), range_list: list):
+        self.range_list = range_list
+        self.df = df.copy()
 
     def create_ind_features(self) -> None:
-        self.create_ind_with_ct('sma', talib.SMA, self.range)
-        self.create_bband_t(self.range)
-        self.create_ind_with_ct('dema', talib.DEMA, self.range)
-        self.create_ind_with_ct('ema', talib.EMA, self.range)
-        self.create_ind_with_ct('kama', talib.KAMA, self.range)
-        self.create_ind_with_ct('t3', talib.T3, self.range)
-        self.create_ind_with_ct('tema', talib.TEMA, self.range)
-        self.create_ind_with_ct('trima', talib.TRIMA, self.range)
-        self.create_ind_with_ct('wma', talib.WMA, self.range)
-        self.create_ind_with_hlct('adx', talib.ADX, self.range)
-        self.create_ind_with_hlct('dmi_up', talib.PLUS_DI, self.range)
-        self.create_ind_with_hlct('dmi_down', talib.MINUS_DI, self.range)
-        self.create_ind_with_ct('cmo', talib.CMO, self.range)
-        self.create_macd()
-        self.create_ind_with_hlct('cci', talib.CCI, self.range)
-        self.create_ind_with_ct('rsi', talib.RSI, self.range)
-        self.create_ind_with_hlct('wllr', talib.WILLR, self.range)
-        self.create_ind_with_hlcvt('mfi', talib.MFI, self.range)
-        self.create_ind_cv('obv', talib.OBV)
-        self.create_ind_hlcv('ad_cha', talib.AD)
-        self.create_ind_with_hlct('atr', talib.ATR, self.range)
-        self.create_ind_with_hlct('natr', talib.NATR, self.range)
-        self.create_ind_with_ct('roc', talib.ROC, self.range)
-        self.create_ind_with_c_stoch(talib.STOCHRSI)
-        self.create_ichmiouk_kijunsen()
-        self.create_ichmiouk_tenkansen()
+        self.create_ind_with_ct(self.df, 'sma', talib.SMA, self.range_list)
+        self.create_bband_t(self.df, self.range_list)
+        self.create_ind_with_ct(self.df, 'dema', talib.DEMA, self.range_list)
+        self.create_ind_with_ct(self.df, 'ema', talib.EMA, self.range_list)
+        self.create_ind_with_ct(self.df, 'kama', talib.KAMA, self.range_list)
+        self.create_ind_with_ct(self.df, 't3', talib.T3, self.range_list)
+        self.create_ind_with_ct(self.df, 'tema', talib.TEMA, self.range_list)
+        self.create_ind_with_ct(self.df, 'trima', talib.TRIMA, self.range_list)
+        self.create_ind_with_ct(self.df, 'wma', talib.WMA, self.range_list)
+        self.create_ind_with_hlct(self.df, 'adx', talib.ADX, self.range_list)
+        self.create_ind_with_hlct(self.df, 'dmi_up', talib.PLUS_DI, self.range_list)
+        self.create_ind_with_hlct(self.df, 'dmi_down', talib.MINUS_DI, self.range_list)
+        self.create_ind_with_ct(self.df, 'cmo', talib.CMO, self.range_list)
+        self.create_macd(self.df)
+        self.create_ind_with_hlct(self.df, 'cci', talib.CCI, self.range_list)
+        self.create_ind_with_ct(self.df, 'rsi', talib.RSI, self.range_list)
+        self.create_ind_with_hlct(self.df, 'wllr', talib.WILLR, self.range_list)
+        self.create_ind_with_hlcvt(self.df, 'mfi', talib.MFI, self.range_list)
+        self.create_ind_cv(self.df, 'obv', talib.OBV)
+        self.create_ind_hlcv(self.df, 'ad_cha', talib.AD)
+        self.create_ind_with_hlct(self.df, 'atr', talib.ATR, self.range_list)
+        self.create_ind_with_hlct(self.df, 'natr', talib.NATR, self.range_list)
+        self.create_ind_with_ct(self.df, 'roc', talib.ROC, self.range_list)
+        self.create_ind_with_c_stoch(self.df, talib.STOCHRSI)
+        self.create_ichmiouk_kijunsen(self.df)
+        self.create_ichmiouk_tenkansen(self.df)
         self.df.ta.fisher(append=True)
         self.df['log_rt'] = self.df.ta.log_return()
 
-    def create_ind_with_ct(self, ind: str, func_ta, range: list) -> None:
-        for i in range:
-            self.df[ind+'_'+str(i)] = func_ta(self.df['Close'], timeperiod=i)
+    def create_ind_with_ct(self, dft: pd.DataFrame(), ind: str, func_ta, range_list: list) -> None:
+        for i in range_list:
+            dft[ind+'_'+str(i)] = func_ta(dft['Close'], timeperiod=i)
 
-    def create_ind_with_c_stoch(self, func_ta) -> None:
-        self.df['stoch_k'], self.df['stoch_d'] = func_ta(self.df['Close'], timeperiod=14, fastk_period=3, fastd_period=3, fastd_matype=0)
+    def create_ind_with_c_stoch(self, dft: pd.DataFrame(), func_ta) -> None:
+        dft['stoch_k'], dft['stoch_d'] = func_ta(dft['Close'], timeperiod=14, fastk_period=3, fastd_period=3, fastd_matype=0)
 
-    def create_ichmiouk_kijunsen(self) -> None:
-        period26_high = self.df['High'].rolling(window=26).max()
-        period26_low = self.df['Low'].rolling(window=26).min()
-        self.df['ich_kline'] = (period26_high + period26_low) / 2
+    def create_ichmiouk_kijunsen(self, dft: pd.DataFrame()) -> None:
+        period26_high = dft['High'].rolling(window=26).max()
+        period26_low = dft['Low'].rolling(window=26).min()
+        dft['ich_kline'] = (period26_high + period26_low) / 2
 
-    def create_ichmiouk_tenkansen(self) -> None:
-        period26_high = self.df['High'].rolling(window=9).max()
-        period26_low = self.df['Low'].rolling(window=9).min()
-        self.df['ich_tline'] = (period26_high + period26_low) / 2
+    def create_ichmiouk_tenkansen(self, dft: pd.DataFrame()) -> None:
+        period26_high = dft['High'].rolling(window=9).max()
+        period26_low = dft['Low'].rolling(window=9).min()
+        dft['ich_tline'] = (period26_high + period26_low) / 2
 
-    def create_ind_with_hlcvt(self, ind: str, func_ta, range: list) -> None:
-        for i in range:
-            self.df[ind+'_'+str(i)] = func_ta(self.df['High'], self.df['Low'], self.df['Close'], self.df['Volume'], timeperiod=i) 
+    def create_ind_with_hlcvt(self, dft: pd.DataFrame(), ind: str, func_ta, range_list: list) -> None:
+        for i in range_list:
+            dft[ind+'_'+str(i)] = func_ta(dft['High'], dft['Low'], dft['Close'], dft['Volume'], timeperiod=i) 
 
-    def create_ind_with_hlct(self, ind: str, func_ta, range: list) -> None:
-        for i in range:
-            self.df[ind+'_'+str(i)] = func_ta(self.df['High'], self.df['Low'], self.df['Close'], timeperiod=i)
+    def create_ind_with_hlct(self, dft: pd.DataFrame(), ind: str, func_ta, range_list: list) -> None:
+        for i in range_list:
+            dft[ind+'_'+str(i)] = func_ta(dft['High'], dft['Low'], dft['Close'], timeperiod=i)
 
-    def create_bband_t(self, range: list, nbdevup=1, nbdevdn=1, matype=0) -> None:
-        for i in range:
-            self.df['upband_'+str(i)], self.df['midband_'+str(i)], self.df['lowband_'+str(i)] = \
-                        talib.BBANDS(self.df['Close'], timeperiod=i, nbdevup=nbdevup, nbdevdn=nbdevdn, matype=matype)
+    def create_bband_t(self, dft: pd.DataFrame(), range_list: list, nbdevup=1, nbdevdn=1, matype=0) -> None:
+        for i in range_list:
+            dft['upband_'+str(i)], dft['midband_'+str(i)], dft['lowband_'+str(i)] = \
+                        talib.BBANDS(dft['Close'], timeperiod=i, nbdevup=nbdevup, nbdevdn=nbdevdn, matype=matype)
 
-    def create_macd(self, fastperiod=12, slowperiod=26, signalperiod=9) -> None:
-        self.df['macd'], self.df['macdsignal'], self.df['macdhist'] = \
-                talib.MACD(self.df['Close'], fastperiod=fastperiod, slowperiod=slowperiod, signalperiod=signalperiod)
+    def create_macd(self, dft: pd.DataFrame(), fastperiod=12, slowperiod=26, signalperiod=9) -> None:
+        dft['macd'], dft['macdsignal'], dft['macdhist'] = \
+                talib.MACD(dft['Close'], fastperiod=fastperiod, slowperiod=slowperiod, signalperiod=signalperiod)
     
-    def create_ind_cv(self, ind: str, func_ta):
-        self.df[ind] = func_ta(self.df['Close'], self.df['Volume'])
+    def create_ind_cv(self, dft: pd.DataFrame(), ind: str, func_ta):
+        dft[ind] = func_ta(dft['Close'], dft['Volume'])
 
-    def create_ind_hlcv(self, ind: str, func_ta):
-        self.df[ind] = func_ta(self.df['High'], self.df['Low'], self.df['Close'], self.df['Volume'])
+    def create_ind_hlcv(self, dft: pd.DataFrame(), ind: str, func_ta):
+        dft[ind] = func_ta(dft['High'], dft['Low'], dft['Close'], dft['Volume'])
