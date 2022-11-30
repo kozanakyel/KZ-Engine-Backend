38662a0b9809e738886468e2fc103c86db045ac4
kozanakyel <kozanakyel@gmail.com>
2022-11-30 12:27:01 +0300

    fetch daily tweets

--- KZ_project_setup/collect_tweets.py
+++ KZ_project_setup/collect_tweets.py
@@ -10,8 +10,8 @@ if __name__ == '__main__':
     test_query = ['agyo', 'sngyo', 'yprak']
 
     
-    for i in query_list_bist:
-        df_tweets = client.get_tweets_with_interval(i, 'tr', hour=24*2, interval=1)
+    for i in test_query:
+        df_tweets = client.get_tweets_with_interval(i, 'tr', hour=24*3, interval=1)
         print(f'shape of {i} tweets df: {df_tweets.shape}')
         path_df = f'./KZ_project_setup/data/tweets_data/{i}/'
         file_df = f'{i}_tweets.csv'
