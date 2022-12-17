c160578a936604c3778d7754501a1d2e5b6e9d89
kozanakyel <kozanakyel@gmail.com>
2022-12-17 17:14:52 +0300

    trying EMA starategy create table for this

--- KZ_project_setup/collect_tweets.py
+++ KZ_project_setup/collect_tweets.py
@@ -9,17 +9,17 @@ if __name__ == '__main__':
     query_list = ['btc','eth','xrp','bnb']
     query_list_bist = ['sumas', 'orma', 'xu100', 'bist', 'sasa']
     test_query = ['agyo', 'sngyo']
-    new_query = ['doge', 'shib', 'ada']
+    new_query = ['shib', 'ada']
 
     
-    for i in query_list_bist:
+    for i in query_list:
         symbol = i
-        df_tweets = client_twitter.get_tweets_with_interval(symbol, 'tr', hour=24*3, interval=4)
+        df_tweets = client_twitter.get_tweets_with_interval(symbol, 'en', hour=24*3, interval=1)
         print(f'shape of {i} tweets df: {df_tweets.shape}')
         path_df = f'./KZ_project_setup/data/tweets_data/{symbol}/'
         file_df = f'{symbol}_tweets.csv'
         #df_tweets = client_twitter.get_tweets_df(symbol, path_df, file_df)
         client_twitter.write_tweets_csv(df_tweets, path_df, file_df)
-        #x, y = tsa.create_sent_results_df(symbol, df_tweets, path_df)
+        x, y = tsa.create_sent_results_df(symbol, df_tweets, path_df)
 
   
