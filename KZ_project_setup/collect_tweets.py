c4f45824969a268b14e0b6d8ca678d48ed79ec0d
kozanakyel <kozanakyel@gmail.com>
2023-01-24 08:01:07 +0300

    create dataSavsaer for factory design patterns

--- KZ_project_setup/collect_tweets.py
+++ KZ_project_setup/collect_tweets.py
@@ -4,18 +4,18 @@ from twitter.tweet_sentiment_analyzer import TweetSentimentAnalyzer
 if __name__ == '__main__':
 
     client_twitter = TwitterCollection()
-    tsa = TweetSentimentAnalyzer(lang='en')
+    tsa = TweetSentimentAnalyzer(lang='tr')
 
     query_list = ['btc','eth','xrp','bnb']
     query_list_bist = ['orma', 'xu100', 'bist', 'sasa','sumas']
-    test_query = ['agyo', 'sngyo']   #########
+    test_query = ['agyo', 'sngyo', 'sumas']   #########
     new_query = ['shib', 'ada', 'doge']
 
     
-    for i in new_query:
+    for i in test_query[1:]:
         symbol = i
         print(f'\n############## SYMBOL - {i}')
-        df_tweets = client_twitter.get_tweets_with_interval(symbol, 'en', hour=24*3, interval=1)
+        df_tweets = client_twitter.get_tweets_with_interval(symbol, 'tr', hour=24*7, interval=1)
         print(f'######## Shape of {i} tweets df: {df_tweets.shape}')
         path_df = f'./KZ_project_setup/data/tweets_data/{symbol}/'
         file_df = f'{symbol}_tweets.csv'
