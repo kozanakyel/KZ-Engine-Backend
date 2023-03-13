1608ec663701efa276a00f848859e7f21a95b950
kozanakyel <kozanakyel@gmail.com>
2023-03-13 11:49:13 +0300

    added and extracted telegram module

--- KZ_project_setup/collect_tweets.py
+++ KZ_project_setup/collect_tweets.py
@@ -4,24 +4,23 @@ from twitter.tweet_sentiment_analyzer import TweetSentimentAnalyzer
 if __name__ == '__main__':
 
     client_twitter = TwitterCollection()
-    tsa = TweetSentimentAnalyzer(lang='tr')
+    tsa = TweetSentimentAnalyzer(lang='en')
 
     query_list = ['btc','eth','xrp','bnb']
-    query_list_bist = ['orma', 'xu100', 'bist', 'sasa','sumas']
-    test_query = ['agyo', 'sngyo', 'sumas']   #########
+    query_list_bist = ['xu100', 'bist', 'sasa','agyo', 'sngyo', 'sumas', 'orma']
     new_query = ['shib', 'ada', 'doge']
     deprem = ['hatay', 'antep', 'maras', 'adiyaman']
     
     
-    for i in deprem:
+    for i in new_query[1:]:
         symbol = i
         print(f'\n############## SYMBOL - {i}')
-        df_tweets = client_twitter.get_tweets_with_interval(symbol, 'tr', hour=24*2, interval=1)
+        df_tweets = client_twitter.get_tweets_with_interval(symbol, 'en', hour=24*7, interval=2)
         print(f'######## Shape of {i} tweets df: {df_tweets.shape}')
         path_df = f'./KZ_project_setup/data/tweets_data/{symbol}/'
         file_df = f'{symbol}_tweets.csv'
         #df_tweets = client_twitter.get_tweets_df(symbol, path_df, file_df)
-        client_twitter.write_tweets_csv(df_tweets, path_df, file_df)
+        #client_twitter.write_tweets_csv(df_tweets, path_df, file_df)
         daily_sents, hourly_sents = tsa.create_sent_results_df(symbol, df_tweets, path_df)
         
 
