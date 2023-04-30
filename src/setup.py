98be66d96d11a9c7d0d343e1addf247d926dfa28
kozanakyel <kozanakyel@gmail.com>
2023-04-30 17:30:30 +0300

    edit setup tool sfor author and license

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup
 
+with open('../LICENSE') as f:
+    license_text = f.read()
+
 setup(
     name="KZ_project",
     version="0.1",
     packages=["KZ_project"],
+    author="Uğur (Kozan) Akyel",
+    author_email="kozanakyel@gmail.com",
+    license=license_text
 )
