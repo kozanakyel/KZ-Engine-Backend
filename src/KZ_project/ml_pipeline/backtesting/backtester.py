c2948068a1e4248440dcc76c65e7667508ae2fdf
kozanakyel <kozanakyel@gmail.com>
2023-05-08 01:17:52 +0300

    edited backtester

--- src/KZ_project/ml_pipeline/backtesting/backtester.py
+++ src/KZ_project/ml_pipeline/backtesting/backtester.py
@@ -13,45 +13,47 @@ class Backtester():
         self.client = client
         self.data_creator = data_creator
         self.period = period
-        self.featured_matrix = self.create_featured_matrix()
+        self.featured_matrix = self._create_featured_matrix()
         self.backtest_data = []
         
-    def create_featured_matrix(self):
+    def _create_featured_matrix(self):
         pipeline = SentimentFeaturedMatrixPipeline(self.data_creator, None, None, is_twitter=False)
         featured_matrix = pipeline.create_sentiment_aggregate_feature_matrix()
         return featured_matrix
         
-    def get_interval_df(self, start_index):
+    def _get_interval_df(self, start_index):
         end_index = self.get_end_index(start_index)
         df = self.featured_matrix.iloc[start_index:end_index]
         return df
     
-    def get_end_index(self, start_index):
+    def _get_end_index(self, start_index):
         return start_index + self.period*24
      
-    def predict_next_hour(self, df):      
+    def _predict_next_hour(self, df):      
         model_engine = ModelEngine(self.data_creator.symbol, None, self.data_creator.source, self.data_creator.interval, is_backtest=True)
         dtt, y_pred, bt_json, acc_score = model_engine.get_accuracy_score_for_xgboost_fit_separate_dataset(df)
         
         return str(dtt + timedelta(hours=int(self.data_creator.interval[0]))), int(y_pred), bt_json, acc_score
     
     def backtest(self, backtest_counts: int):
-        fm = self.create_featured_matrix()
+        fm = self.featured_matrix
         true_accuracy = []
         false_accuracy = []
         succes_predict = 0
         for i in range(backtest_counts):
-            df = self.get_interval_df(i)
-            dt, signal, bt, accuracy_score = self.predict_next_hour(df)
-            ei = self.get_end_index(i)
+            df = self._get_interval_df(i)
+            dt, signal, bt, accuracy_score = self._predict_next_hour(df)
+            ei = self._get_end_index(i)
             actual_result = (fm.loc[fm.index[ei+1]]["log_return"] > 0).astype(int)
+            # log_return = fm.loc[fm.index[ei+1]]["log_return"]
             self.backtest_data.append((dt, accuracy_score, signal, actual_result))
+            
             if actual_result == signal:
                 succes_predict = succes_predict + 1
                 true_accuracy.append(accuracy_score)
             else:
                 false_accuracy.append(accuracy_score)
-        return succes_predict / len(self.backtest_data), sum(true_accuracy)/len(true_accuracy), sum(false_accuracy)/len(false_accuracy)
+        return succes_predict / len(self.backtest_data)
     
 
 if __name__ == '__main__':
