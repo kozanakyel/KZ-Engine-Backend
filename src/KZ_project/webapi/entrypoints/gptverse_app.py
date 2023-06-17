16c48f482e705407d980ba6541cd3bfdc1a76967
kozanakyel <kozanakyel@gmail.com>
2023-06-17 12:11:18 +0300

    delted unnecessary andpoint for gpt and add new report for ai assistant

--- src/KZ_project/webapi/entrypoints/gptverse_app.py
+++ src/KZ_project/webapi/entrypoints/gptverse_app.py
@@ -10,29 +10,6 @@ load_dotenv()
 
 gpt_blueprint = Blueprint('gptverse', __name__)
 
-openai_api_key = os.getenv('OPENAI_API_KEY')
-chatgpt = OpenAI(
-    model_name='text-ada-001',
-    temperature=0.3,
-    max_tokens=150
-)
-
-
-@gpt_blueprint.route('/llm_response', methods=['POST'])
-def post_question_to_llm():
-    # Get the question from the request
-    question = request.json['question']
-
-    # Perform the OpenAI API call
-    result = chatgpt(question)
-
-    # Extract the numbers and newlines
-    pattern = r'\n\d+\. '  # Matches newlines followed by numbers and a dot
-    extract_n = re.sub(pattern, '', result)
-
-    # Return the extracted response as JSON
-    return jsonify({'response': extract_n[2:]})
-
 
 @gpt_blueprint.route('/ai_project_assistant', methods=['POST'])
 def post_assistant_response():
