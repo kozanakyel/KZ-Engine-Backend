00b0becea6ec13df9921dcc80dc454dbd3982650
kozanakyel <kozanakyel@gmail.com>
2023-07-01 17:02:02 +0300

    edited trading advisor but we have problem about detected price understang for support and resistance

--- src/KZ_project/Infrastructure/services/gptverse_ai_assistant/trading_advisor.py
+++ src/KZ_project/Infrastructure/services/gptverse_ai_assistant/trading_advisor.py
@@ -22,36 +22,40 @@ openai = OpenAI(
 
 # create our examples
 trend_examples = [
-{
-    "query": f"Ichmouku trend,Strong Bullish,Detected price,110.34,Supertrend,uptrend,"
-                f" cross for supertrend,True,Current Price,95.123"
-                f" What is your advice for trading for this trend and price values?",
-    "answer": "Our Ichmouku trend is Strong bullish that is good for next time step, we can say 110.34 our resistance \
-        because the detected price from ichmouku is greater than our current price 95.123. Also we have \
-        croos line and trend changing step for our supertrend indicator within 5 hour and the supertrend indicator momuntum is uptrend. \
-            You can hold if you have this coin. Or you can buy this coin because we have trend changing for uptrend \
-                and have a strong bullish momentum from ichmouku. you can follow this holding opeartion \
-                    our detected resistance price to 110.34"
-}, {
-    "query": f"Ichmouku trend,Bearish,Detected price,90.34,Supertrend,downtrend,"
-                f" cross for supertrend,No,Current Price,95.123"
-                f" What is your advice for trading for this trend and price values?",
-    "answer": "Our ichmouku trend is bearish not string enough, detected price is 90.34 and we can say this is our \
-        supported price because current price is greater than our detected price. Also we have a downtrend for supertrend indicator. \
-            we can say you can wait for buying because we dont see any cross in supertrend or bullish movement \
-                from our trade engine."
-}
+    {
+        "query": f"Ichmouku trend,Strong Bullish,Detected price,110.34,Supertrend,uptrend,"
+                    f" cross for supertrend,True,Current Price,95.123"
+                    f" What is your advice for trading for this trend and price values?",
+        "answer": "Our Ichmouku trend is Strong bullish that is good for next time step, we can say 110.34 our resistance \
+            because the detected price from ichmouku is greater than our current price 95.123. Also we have \
+            croos line and trend changing step for our supertrend indicator within 5 hour and the supertrend indicator momuntum is uptrend. \
+                You can hold if you have this coin. Or you can buy this coin because we have trend changing for uptrend \
+                    and have a strong bullish momentum from ichmouku. you can follow this holding opeartion \
+                        our detected resistance price to 110.34"
+    }, {
+        "query": f"Ichmouku trend,Bearish,Detected price,90.34,Supertrend,downtrend,"
+                    f" cross for supertrend,No,Current Price,95.123"
+                    f" What is your advice for trading for this trend and price values?",
+        "answer": "Our ichmouku trend is bearish not string enough, detected price is 90.34 and we can say this is our \
+            supported price because current price is greater than our detected price. Also we have a downtrend for supertrend indicator. \
+                we can say you can wait for buying because we dont see any cross in supertrend or bullish movement \
+                    from our trade engine."
+    }
 ]
 
 trend_prefix = """Answer the question based on the context below.
-You are the trading advisor. Also you are expert on Supertrend and Ichmouku Cloud indicators.
+    You are the trading advisor. Also you are expert on Supertrend and Ichmouku Cloud indicators.
 
-Context: supertrend has 2 level: uptrend and downtrend. Ichmouku has 4 level Bullish, strong Bullish,
-Bearish, Strong Bearish. our detected price is coming from ichmouku indicator and we can use support or resistance price.
-If the detected price is lower than the current Price this detected price is support price if detected price is greater than current price the detected price is resistance level.
-Dont forget you can find which price is gretaer or lower than the other between detected price and current price.
-you must a evaluation to customer current stiation from this indicator values. then you should give a trading advice to custormer.
-Here are some examples:
+    Context: supertrend has 2 level: uptrend and downtrend. Ichmouku has 4 level Bullish, strong Bullish,
+    Bearish, Strong Bearish. our detected price is coming from ichmouku indicator and the detected price 
+    actually represent support or resistance price.
+    If the detected price is lower than the current Price this detected price is support price 
+    if detected price is greater than current price the detected price is resistance level.
+    Dont forget you can define support or resistance price via the detected price. And you must define before the advice 
+    detected price greater or lower than the current price.
+    you must give a evaluation to customer current situation from this indicator values. 
+    then you must give a trading advice to custormer.
+    Here are some examples:
 """
 
 # create our examples
@@ -94,24 +98,24 @@ example_prompt = PromptTemplate(
 # now break our previous prompt into a prefix and suffix
 # the prefix is our instructions
 rsi_prefix = """Answer the question based on the context below.
-You are the trading advisor. Also you are expert on RSI, MFI and DMP, DMI indicators.
+    You are the trading advisor. Also you are expert on RSI, MFI and DMP, DMI indicators.
 
-Context: RSI indicator value range betwwen 0-100. RSI value 70 and above meaning that overbought area.
-So you should sell your coin. also value 80 is a extreme overbought and you act cautiously.
-RSI value 30 that meaning is overselling area. and value 20 is extreme overselling are.
-if the RSI value are 20-30s you should bought this coin. 30-70 range waiting or
-you can look other indicator results.
-MFI indicator value range betwen 0-100. MFI value 80 and above meaning that overbought area.
-So you should sell your coin. also value 90 is a extreme overbought and you should act cautiously.
-MFI value 20 that meaning is overselling area. and value 10 is extreme overselling are.
-if the MFI value are 10-20s you should bought this coin. 20-80 range waiting or
-you can look other indicator results.
-Else it shows overselling condition between 0-25.
-DMI indicator is a collection of indicators including DMP, DMI, and ADX. The Plus Direction Indicator DMP and
-Minus Direction Indicator DMI show the current price direction. When the DMP is above DMN,
-the current price momentum is up. When the DMN is above DMP, the current price momentum is down.
-ADX measures the strength of the trend, either up or down; a reading above 25 indicates a strong trend.
-Here are some examples:
+    Context: RSI indicator value range betwwen 0-100. RSI value 70 and above meaning that overbought area.
+    So you should sell your coin. also value 80 is a extreme overbought and you act cautiously.
+    RSI value 30 that meaning is overselling area. and value 20 is extreme overselling are.
+    if the RSI value are 20-30s you should bought this coin. 30-70 range waiting or
+    you can look other indicator results.
+    MFI indicator value range betwen 0-100. MFI value 80 and above meaning that overbought area.
+    So you should sell your coin. also value 90 is a extreme overbought and you should act cautiously.
+    MFI value 20 that meaning is overselling area. and value 10 is extreme overselling are.
+    if the MFI value are 10-20s you should bought this coin. 20-80 range waiting or
+    you can look other indicator results.
+    Else it shows overselling condition between 0-25.
+    DMI indicator is a collection of indicators including DMP, DMI, and ADX. The Plus Direction Indicator DMP and
+    Minus Direction Indicator DMI show the current price direction. When the DMP is above DMN,
+    the current price momentum is up. When the DMN is above DMP, the current price momentum is down.
+    ADX measures the strength of the trend, either up or down; a reading above 25 indicates a strong trend.
+    Here are some examples:
 """
 # and the suffix our user input and output indicator
 suffix = """
